14c7a64c19bd7d1d2f4effe9afc574a30fdbb3cc
Lwq <127438227+Why12389@users.noreply.github.com>
2023-09-19 13:17:45 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "http://nowgg.me/";//your website url
+  let target = "https://www.xbox.com/en-us/play/games/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||

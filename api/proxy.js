3d689334a29e5373b7a047e14d6af97a5e7ebeee
Lwq <127438227+Why12389@users.noreply.github.com>
2023-09-19 12:46:52 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://google-gules-six.vercel.app/url?sa=i&rct=j&q=&esrc=s&source=web&cd=&ved=0CAMQw7AJahcKEwjQ6o6qm7eBAxUAAAAAHQAAAAAQAg&url=https%3A%2F%2Fnow.gg%2F&psig=AOvVaw1enJXKHD_FZCRSOYWo5kyl&ust=1695231425859633&opi=89978449";//your website url
+  let target = "https://now.gg/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
